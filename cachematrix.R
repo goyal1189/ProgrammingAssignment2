f2fe4f152489711c7d9f77c22031a947a5c9268e
ggoyal <gauravgoyal1189@gmail.com>
2014-04-26 16:29:49 +0530

    Added Comments

--- cachematrix.R
+++ cachematrix.R
@@ -26,8 +26,8 @@ i <- NULL
 ## This Function Will check whether the cache memory does contains the inverse for the
 ## Matrix or not. If Yes it will return, the Inverse of a matrix from Cache Memory else will calculate Inverse and will show the result
 
-cacheSolve <- function(x, ...) {                      ## Please enter the Argument in cacheSolve function of the form "makeCacheMatrix(matrix(data,nrow,nrow))" 
-      
+cacheSolve <- function(x, ...) {                      ## Please enter the Argument in cacheSolve function of the form  
+      						      ## "makeCacheMatrix(matrix(data,nrow,nrow))"
    m <- x$getinverse()
 
   	if(!is.null(m)) {
