60bda2e1c4b26d455b5c51fe0748af2e1c828faa
ggoyal <gauravgoyal1189@gmail.com>
2014-04-26 16:28:11 +0530

    Added comment for passing the Argument in Function cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -26,14 +26,17 @@ i <- NULL
 ## This Function Will check whether the cache memory does contains the inverse for the
 ## Matrix or not. If Yes it will return, the Inverse of a matrix from Cache Memory else will calculate Inverse and will show the result
 
-cacheSolve <- function(x, ...) {
-       m <- x$getinverse()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setinverse(m)
-return(m)
+cacheSolve <- function(x, ...) {                      ## Please enter the Argument in cacheSolve function of the form "makeCacheMatrix(matrix(data,nrow,nrow))" 
+      
+   m <- x$getinverse()
+
+  	if(!is.null(m)) {
+    			message("getting cached data")
+  		        return(m)
+  		  	}
+   data <- x$get()
+   m <- solve(data, ...)
+   x$setinverse(m)
+ return(m)
+
 }
